a0e2d62b456d0007a19f333ad530feee99c62b39
Toria <itoria@foxmail.com>
2022-09-22 11:35:17 +0800

    商家销售统计完成

--- src/components/mySeller.vue
+++ src/components/mySeller.vue
@@ -18,15 +18,79 @@ export default {
   mounted () {
     this.initChart()
     this.getData()
+    window.addEventListener('resize', this.screenAdapter)
+    // 主动进行适配
+    this.screenAdapter()
   },
   destroyed () {
     // 销毁定时器
     clearInterval(this.timer)
+    // 销毁监听器
+    window.removeEventListener('resize', this.screenAdapter)
   },
   methods: {
     // 初始化echart实例对象
     initChart () {
-      this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+      this.chartInstance = this.$echarts.init(this.$refs.seller_ref, 'chalk')
+      // 对图标初始化的控制
+      const initOption = {
+        title: {
+          text: '‖ 商家销售统计',
+          left: 20,
+          top: 20
+        },
+        grid: {
+          top: '20%',
+          left: '3%',
+          right: '6%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'line',
+            z: 0,
+            lineStyle: {
+              color: '#2D3443'
+            }
+          }
+        },
+        series: [
+          {
+            type: 'bar',
+            label: {
+              show: true,
+              position: 'right',
+              textStyle: {
+                color: 'white'
+              }
+            },
+            itemStyle: {
+              // 指明颜色渐变的方向
+              // 指明不同百分比之下颜色的值
+              color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                {
+                  offset: 0,
+                  color: '#5052EE'
+                },
+                // 百分之100状态下的颜色
+                {
+                  offset: 1,
+                  color: '#AB6EE5'
+                }
+              ])
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(initOption)
     },
     // 获取服务器数据
     async getData () {
@@ -55,22 +119,17 @@ export default {
       const sellerValues = this.showDate.map((item) => {
         return item.value
       })
-      const option = {
-        xAxis: {
-          type: 'value'
-        },
+      const dataOption = {
         yAxis: {
-          type: 'category',
           data: sellerNames
         },
         series: [
           {
-            type: 'bar',
             data: sellerValues
           }
         ]
       }
-      this.chartInstance.setOption(option)
+      this.chartInstance.setOption(dataOption)
     },
     startInterval () {
       if (this.timer) {
@@ -83,6 +142,37 @@ export default {
         }
         this.updateChart()
       }, 2000)
+    },
+    // 当浏览器窗口发生变化时，会调用方法来完成屏幕的适配
+    screenAdapter () {
+      const titleFontSize = this.$refs.seller_ref.offsetWidth / 100 * 3.5
+      // 分辨率大小相关的配置
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        tooltip: {
+          axisPointer: {
+            lineStyle: {
+              width: titleFontSize,
+              color: '#2D3443'
+            }
+          }
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0]
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(adapterOption)
+      // 手动调用图标的resize方法才能有效果
+      this.chartInstance.resize()
     }
   }
 }
